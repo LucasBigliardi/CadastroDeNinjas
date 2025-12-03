33b9f270260425d01601527ac679dec3fbe54179
LucasBigliardi <86639865+LucasBigliardi@users.noreply.github.com>
2025-12-03 10:09:53 -0300

    Modificação do codigo para utilização de Lombok (Anottations), utilizadas para fazer @AllArgsConstructor,@NoArgsConstructor, Getter e Setters (@Data)

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeNinjas.Missoes;
 
 import dev.java10x.CadastroDeNinjas.Ninjas.Controller.Service.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table (name = "tb_missoes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class MissoesModel {
 
@@ -20,10 +26,6 @@ public class MissoesModel {
     //@OneToMany uma missao pode ter varios ninjas
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninja;
-
-    public MissoesModel(){
-
-    }
 
     public MissoesModel(String nomeMissao, String dificuldade){
         this.nomeMissao = nomeMissao;

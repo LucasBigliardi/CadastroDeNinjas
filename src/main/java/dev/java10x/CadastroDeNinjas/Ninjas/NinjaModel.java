72d9da1f238041600ced214540c59ff353e013a8
LucasBigliardi <86639865+LucasBigliardi@users.noreply.github.com>
2026-02-09 13:54:58 -0300

    fix: adiciona o @Column na classe

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -15,10 +15,20 @@ import lombok.NoArgsConstructor;
 public class NinjaModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "id")
     private Long id;
+
+    @Column (name = "nome")
     private String nome;
+
+    @Column (name = "idade")
     private int idade;
+
+    @Column(unique = true)
     private String email;
+
+    @Column (name = "img_url")
+    private String imgUrl;
 
     //@ManyToOne um ninja tem uma unica missao
     @ManyToOne
